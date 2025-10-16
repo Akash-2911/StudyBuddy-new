a32b0beb9edd54b80661c8136fd22fb88aada4ac
Akash-2911 <2911akashpatel@gmail.com>
2025-10-16 12:29:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+// Dynamically load common footer
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer-container").innerHTML = data;
+    });
+});
